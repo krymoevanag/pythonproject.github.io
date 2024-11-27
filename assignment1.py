b80f7a9ab136c5acf32e21e8d8ff12192472773b
Kirimi <128530618+krymoevanag@users.noreply.github.com>
2024-11-27 22:58:37 +0300

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -3,17 +3,17 @@ operation= input("Enter the operation(+,-,*,/)")
 num2=float(input("Enter the second number: "))
 if operation=="+":
     result= num1+num2
-    print(f"The result of {num1} + {num2} is: {result}")
+    print(f"{num1} + {num2} ={result}")
 elif operation == "-":
     result = num1 - num2
-    print(f"The result of {num1} - {num2} is: {result}")
+    print(f"{num1} - {num2} ={result}")
 elif operation == "*":
     result = num1 * num2
-    print(f"The result of {num1} * {num2} is: {result}")
+    print(f"{num1} * {num2} = {result}")
 elif operation == "/":
     if num2 != 0:  # Checking for division by zero
         result = num1 / num2
-        print(f"The result of {num1} / {num2} is: {result}")
+        print(f"{num1} / {num2} ={result}")
     else:
         print("Error: Division by zero is not allowed!")
 else:
